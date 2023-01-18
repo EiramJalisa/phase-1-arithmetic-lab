2755c41e7bb7b95ef08fb7bae9d9e016f98a01e9
+ <+>
2023-01-17 19:21:41 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1= 2
+let num2= 31
+let multiply = num1 * num2
 
-// Write your code here
+let random= 4
+Math.random();
+
+let num3= 6
+let num4= 4
+let mod= (num3,num4);
+
+let max= 20
+Math.max(1, 2, 16, 4, 20);
